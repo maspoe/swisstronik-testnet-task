a81bf967212633c6d843accc0c6adb081935f5c2
Purwanto . <kontakmaspoe@gmail.com>
2024-07-24 04:19:10 +0000

    Edit hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["e5ec3ae9ae0e5aa00b5c4ae9375e7204955ec0378b26cf74025b54b28e383186"], //Your private key starting with "0x" 
+      accounts: ["0x${process.env.PRIVATE_KEY}"], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
